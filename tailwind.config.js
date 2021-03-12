f8707c4f4e3947ec54ae8cc3b9b35e4bf2a3a71c
Jake <66141166+harvanchik@users.noreply.github.com>
2021-03-11 21:37:46 -0800

    added landing page; sliding show background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetbrains: ["'JetBrains Mono'", 'monospace']
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-5deg)' },
+          '50%': { transform: 'rotate(5deg)' },
+        }
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['hover'],
+    },
   },
   plugins: [],
 };
